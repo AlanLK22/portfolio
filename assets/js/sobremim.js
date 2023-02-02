ca2851560186def09b52d7ecfe92cad7cb00df05
alanLK22 <alansenawow@gmail.com>
2023-02-02 12:47:48 -0300

    atualização de estilos e adição de conteúdos novos

---
+++ assets/js/sobremim.js
@@ -0,0 +1,30 @@
+const card = document.querySelector('.sobre-mim')
+
+const sobreMim = () => {
+
+return {
+    nome: 'Alan sena',
+    idade: 25,
+    imagem: './assets/img/perfil.jpeg',
+    bio: 'Sou um estudante autodidata de programação, atualmente estudando javascript e typescript, amo tecnologia e sua capacidade de mudar o mundo!'
+}
+
+
+}
+
+
+
+const infomações = `<div class=card>
+
+<h1>${sobreMim().nome}</h1>
+<img src=${sobreMim().imagem}></img>
+
+<p> <b>idade:</b> ${sobreMim().idade} anos</p>
+
+<h2>Sobre mim</h2>
+
+<p class=bio>${sobreMim().bio}</p>
+
+</div>`
+
+card.innerHTML = infomações
